7ce188aa654b03d3fcaf505a142ce9f15712e6e6
Cam <cameron.vs.zombies@gmail.com>
2025-09-08 14:51:10 -0700

    Update Main.lua

--- X-Assignments/X-BalatroMod-Cam/Main.lua
+++ X-Assignments/X-BalatroMod-Cam/Main.lua
@@ -1,1 +1,17 @@
+local NFS = require("nativefs")
+to_big = to_big or function(a) return a end
+lenient_bignum = lenient_bignum or function(a) return a end
 
+local function load_jokers_folder()
+    local mod_path = SMODS.current_mod.path
+    local jokers_path = mod_path .. "/jokers"
+    local files = NFS.getDirectoryItemsInfo(jokers_path)
+    for i = 1, #files do
+        local file_name = files[i].name
+        if file_name:sub(-4) == ".lua" then
+            assert(SMODS.load_file("jokers/" .. file_name))()
+        end
+    end
+end
+
+load_jokers_folder()
